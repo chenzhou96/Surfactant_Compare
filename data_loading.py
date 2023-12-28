ea89c9acab96a6f7482b815ec1f8a09fe3a49870
unknown <06427@vz.domain.com.ad>
2023-12-28 18:06:04 +0800

    algorithm optimizing v0.1

--- data_loading.py
+++ data_loading.py
@@ -12,7 +12,7 @@ def _read_filenames(path: Path) -> list:
     filenames = os.listdir(path)
 
     # 只读取csv文件
-    names_regex = re.compile(r'.csv$')
+    names_regex = re.compile(r'\.csv$')
     name_output = re.compile(r'^output')
     for name in filenames[:]:
         if not names_regex.search(name.lower()):
@@ -20,37 +20,44 @@ def _read_filenames(path: Path) -> list:
         if name_output.search(name.lower()):
             filenames.remove(name)
 
-    return filenames
+    if filenames:
+        return filenames
+    else:
+        print(f'**********\nNo csv file in {path}\n**********')
+        exit()
 
 def _read_csv_data(path: Path, filename: str, column_index: int, skip_row: int) -> np.ndarray:
     """
     读取csv文件内第row行的数据
-    返回ndarray数组
+    返回二维ndarray数组
     """
     column_index -= 1
     try:
         path = str(path / filename)
-        return np.loadtxt(path, delimiter=',', skiprows=skip_row, usecols=(column_index,)) # delimiter根据文件格式修改
-    except:
+        return np.loadtxt(path, delimiter=',', skiprows=skip_row, usecols=(0, column_index)) # delimiter根据文件格式修改
+    except Exception as error:
+        print(f'**********\n{filename} read failed\nError: {error}\n**********')
         return None
     
 def _read_PDA_max_data(path: Path, filename: str, skip_row: int) -> np.ndarray:
     """
     读取PDA光谱文件
-    返回二维矩阵ndarray数组
+    返回二维ndarray数组
     """
     try:
         path = str(path / filename)
         data = np.loadtxt(path, delimiter=',', skiprows=skip_row) # delimiter根据文件格式修改
         # data为二维数组 第一列为时间 其他列为不同波段的吸光度
-    except:
+    except Exception as error:
+        print(f'**********\n{filename} read failed\nError: {error}\n**********')
         return None
     
     rows = data.shape[0]
-    PDA_data = np.empty(rows)
+    PDA_data = np.empty((rows, 2))
+    PDA_data[:, 0] = data[:, 0]
     for index in range(rows):
         intensity = data[index][1:]
-        PDA_data[index] = intensity.max() # 有待和手动处理的数据对比验证一下
+        PDA_data[index, 1] = intensity.max() # 有待和手动处理的数据对比验证一下
 
     return PDA_data
 
@@ -60,21 +67,42 @@ def _zip_data(csvdata: np.ndarray, n = 1000) -> np.ndarray:
     返回横坐标数据点为n的新数据
     """
 
-    rows = len(csvdata)
+    rows = csvdata.shape[0]
 
     # 如果数据点不满n个 不需要进行压缩
     if n > rows:
         return csvdata
 
-    zipdata = np.empty(n)
+    zipdata = np.empty((n, 2))
     for step in range(n):
         start = int(step * rows / n)
         end = int((step + 1) * rows / n)
-        zipdata[step] = csvdata[start: end].mean()
+        zipdata[step, 0] = csvdata[start: end, 0].mean()
+        zipdata[step, 1] = csvdata[start: end, 1].mean()
 
     return zipdata
 
-def read_all_data(path: Path, column_index: int, skip_row: int, flag: str) -> dict:
+def _select_x_axis(csvdata: np.ndarray, x_axis: tuple) -> np.ndarray:
+    """
+    选择谱图的x轴范围
+    返回二维ndarray数组
+    """
+
+    x_left, x_right = x_axis[0], x_axis[1]
+    
+    if x_left > x_right:
+        x_max, x_min = x_left, x_right
+    else:
+        x_max, x_min = x_right, x_left
+
+    new_csvdata = list()
+    for item in csvdata:
+        if item[0] > x_min and item[0] < x_max:
+            new_csvdata.append(item)
+    
+    return np.array(new_csvdata)
+
+def read_all_data(path: Path, column_index: int, skip_row: int, flag: str, x_axis: tuple) -> dict:
     """
     读取路径文件夹内所有csv文件内容
     返回文件名和文件内容构成的字典
@@ -82,20 +110,30 @@ def read_all_data(path: Path, column_index: int, skip_row: int, flag: str) -> di
     
     alldata = dict()
     filenames = _read_filenames(path)
+
     if flag == 'nmr' or 'PDA_single':
         for filename in filenames:
-            alldata[filename] = _zip_data(_read_csv_data(path, filename, column_index, skip_row)) # column根据文件格式修改
+            single_data = _select_x_axis(_read_csv_data(path, filename, column_index, skip_row)) # column根据文件格式修改
+            alldata[filename] = single_data
     elif flag == 'PDA_max':
         for filename in filenames:
-            alldata[filename] = _zip_data(_read_PDA_max_data(path, filename, skip_row))
+            single_data = _select_x_axis(_read_PDA_max_data(path, filename, skip_row))
+            alldata[filename] = single_data
     else:
-        return None
+        print(f'**********\n{flag}格式不存在 请重新输入参数\n**********')
+        exit()
     
     return alldata
 
 if __name__ == "__main__":
     pass
 
-    # path = Path('/Users/zhouchen/Documents/CS_project/Surfactant_Compare')
-    # a = read_all_data(path)
-    # print(a['test.csv'][1])
+    path = Path('C:\\Users\\06427\\Desktop\\Tween 80  原始数据CSV')
+    filename = '1.csv'
+    zip_data = _zip_data(_select_x_axis(_read_csv_data(path, filename, 2, 0), (6, -0.1)))
+    zip_data_path = path / 'select_data_zip1000.csv'
+    import csv
+    with open(zip_data_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for item in zip_data:
+            writer.writerow(item)
--- run.py
+++ run.py
@@ -8,15 +8,20 @@ if __name__ == '__main__':
 
 # 参数设置
 ##注意事项：
-##1. 文件为csv格式 数据间隔符号为,
-    PATH = "C:\\Users\\06427\\Desktop\\项目_试剂化学成分对比V1.0\\数据分析算法\\Triton"
+##1. 文件为csv格式 数据间隔符号为英文逗号(,) 数据第一列必须代表谱图x轴
+    PATH = "C:\\Users\\06427\\Desktop\\Tween 80  原始数据CSV"
     # PATH读取指定的文件夹位置 windows系统下需要使用\\代替路径中的\
-    FLAG = 'PDA_single'
+    FLAG = 'nmr'
     # FLAG单引号内可选内容为 PDA_max, PDA_single, nmr 分别对应PDA最大值光谱,PDA单波长光谱,核磁谱图
-    COLUMN_INDEX = 2 #固定不需要输入
+    SKIP_ROW = 0
+    # SKIP_ROW表示需要跳过的数据开头行数
+    COLUMN_INDEX = 2
     # 当FLAG内容为PDA_single或nmr时 COLUMN_INDEX需要调整到数据所在列 比如数据在第二列则COLUMN_INDEX = 2
-    SKIP_ROW = 2 #固定不需要输入
-    # 当FLAG内容为PDA_max时 SKIP_ROW表示需要跳过的数据开头行数
+    X_LEFT = 6
+    X_RIGHT = -0.1
+    # X_LEFT和X_RIGHT框选出谱图需要进行对比的x轴范围
+    X_LABEL = 0
+    # X_LABEL用于作为基准点对齐谱图 比如nmr数据中选择化学位移为0的TMS峰 液相色谱数据中选择溶剂峰/倒峰的出峰时间
 # 设置结束
 
 def _output_name(path: Path, data_name: str) -> Path:
@@ -36,7 +41,7 @@ if __name__ == '__main__':
     
     path = Path(PATH)
 
-    origin_data = dl.read_all_data(path, column_index=COLUMN_INDEX, skip_row=SKIP_ROW, flag=FLAG)
+    origin_data = dl.read_all_data(path, column_index=COLUMN_INDEX, skip_row=SKIP_ROW, flag=FLAG, x_axis=(X_LEFT, X_RIGHT))
     if origin_data:
         results = sc.pairwise_comparsion(origin_data)
         compared_data_path = _output_name(path, 'output_compared_data')
@@ -47,9 +52,9 @@ if __name__ == '__main__':
                 sample_A, sample_B = key.split(',')
                 writer.writerow([sample_A, sample_B, result])
 
-        print(f'相似度分析完成!\n数据保存在{compared_data_path}\n')
+        print(f'**********\n相似度分析完成!\n数据保存在{compared_data_path}\n**********')
     else:
-        print("未读取到有效数据!\n")
+        print("**********\n未读取到有效数据!\n**********")
         exit()
 
     pca_data = PCA.PCA_analysis(str(compared_data_path))
@@ -61,8 +66,7 @@ if __name__ == '__main__':
             for sample_name, signgle_pca_data in zip(*pca_data):
                 writer.writerow([sample_name, *signgle_pca_data])
         
-        print(f'PCA分析完成!\n数据保存在{pca_data_path}\n')
-
+        print(f'**********\nPCA分析完成!\n数据保存在{pca_data_path}\n**********')
     else:
-        print("PCA分析失败!\n")
+        print("**********\nPCA分析失败!\n**********")
         exit()
